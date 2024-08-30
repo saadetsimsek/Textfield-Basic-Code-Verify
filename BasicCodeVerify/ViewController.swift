43a943c55867ca706f056fd73bdbc709e74a6947
saadetsimsek <saadetsimsek13@gmail.com>
2024-08-30 15:15:08 +0300

    Initial Commit

---
+++ BasicCodeVerify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicCodeVerify
+//
+//  Created by Saadet Şimşek on 30/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
